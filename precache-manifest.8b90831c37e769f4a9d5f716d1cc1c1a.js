6dc3c4e865dba4fcd479fe94d99ee641d2123b1b
EINazare <emanuelioannazare@gmail.com>
2020-09-01 18:37:23 +0300

    Updates

---
+++ precache-manifest.8b90831c37e769f4a9d5f716d1cc1c1a.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "626ab116100b5b59fedc1b1b5828d328",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9643947f3cd10458c34b",
+    "url": "/static/css/2.dcc89689.chunk.css"
+  },
+  {
+    "revision": "0820dc1b5057b8111c42",
+    "url": "/static/css/main.ad8e3958.chunk.css"
+  },
+  {
+    "revision": "9643947f3cd10458c34b",
+    "url": "/static/js/2.5f8bfecb.chunk.js"
+  },
+  {
+    "revision": "e30432c97c11cf3ee19d8b38067db5ff",
+    "url": "/static/js/2.5f8bfecb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0820dc1b5057b8111c42",
+    "url": "/static/js/main.8e8cbcdb.chunk.js"
+  },
+  {
+    "revision": "5fd7e63070b3aabbee72",
+    "url": "/static/js/runtime-main.af28ebdd.js"
+  },
+  {
+    "revision": "368685db949cddcd901ba54559a727d9",
+    "url": "/static/media/angular.368685db.jpg"
+  },
+  {
+    "revision": "ab904daa548967670847fc3929bf50f4",
+    "url": "/static/media/bootstrap.ab904daa.jpg"
+  },
+  {
+    "revision": "085b1dd8427dbeff10bd55410915a3f6",
+    "url": "/static/media/fa-brands-400.085b1dd8.ttf"
+  },
+  {
+    "revision": "0fabb6606be4c45acfeedd115d0caca4",
+    "url": "/static/media/fa-brands-400.0fabb660.eot"
+  },
+  {
+    "revision": "cac68c831145804808381a7032fdc7c2",
+    "url": "/static/media/fa-brands-400.cac68c83.woff2"
+  },
+  {
+    "revision": "ccfdb9dc442be0c629d331e94497428b",
+    "url": "/static/media/fa-brands-400.ccfdb9dc.svg"
+  },
+  {
+    "revision": "dc0bd022735ed218df547742a1b2f172",
+    "url": "/static/media/fa-brands-400.dc0bd022.woff"
+  },
+  {
+    "revision": "05b53beb21e3ef13d28244545977152d",
+    "url": "/static/media/fa-regular-400.05b53beb.woff"
+  },
+  {
+    "revision": "1a78af4105d4d56e6c34f76dc70bf1bc",
+    "url": "/static/media/fa-regular-400.1a78af41.ttf"
+  },
+  {
+    "revision": "3a3398a6ef60fc64eacf45665958342e",
+    "url": "/static/media/fa-regular-400.3a3398a6.woff2"
+  },
+  {
+    "revision": "ad3a7c0d77e09602f4ab73db3660ffd8",
+    "url": "/static/media/fa-regular-400.ad3a7c0d.eot"
+  },
+  {
+    "revision": "e75dfd904d366a2560c63c23cfc98ef8",
+    "url": "/static/media/fa-regular-400.e75dfd90.svg"
+  },
+  {
+    "revision": "03ba7cb710104df27f1c9c46d64bee4e",
+    "url": "/static/media/fa-solid-900.03ba7cb7.svg"
+  },
+  {
+    "revision": "781e85bb50c8e8301c30de56b31b1f04",
+    "url": "/static/media/fa-solid-900.781e85bb.ttf"
+  },
+  {
+    "revision": "89bd2e38475e441a5cd70f663f921d61",
+    "url": "/static/media/fa-solid-900.89bd2e38.eot"
+  },
+  {
+    "revision": "c500da19d776384ba69573ae6fe274e7",
+    "url": "/static/media/fa-solid-900.c500da19.woff2"
+  },
+  {
+    "revision": "ee09ad7553b8ad3d81150d609d5341a0",
+    "url": "/static/media/fa-solid-900.ee09ad75.woff"
+  },
+  {
+    "revision": "4ffd4fe7945af123788bf5888296c696",
+    "url": "/static/media/github.4ffd4fe7.svg"
+  },
+  {
+    "revision": "87be59a1f7061fc6021876aad6fee028",
+    "url": "/static/media/google.87be59a1.svg"
+  },
+  {
+    "revision": "c4eb3d0be0cf5a2d156a123bdf7fb40d",
+    "url": "/static/media/react.c4eb3d0b.jpg"
+  },
+  {
+    "revision": "2fee0b5056301e6c2a5bc8df39c37854",
+    "url": "/static/media/register_bg_2.2fee0b50.png"
+  },
+  {
+    "revision": "e7c82e5c569db9d9761b13ef1384f702",
+    "url": "/static/media/sketch.e7c82e5c.jpg"
+  },
+  {
+    "revision": "53033970a416368da35794389680266f",
+    "url": "/static/media/team-1-800x800.53033970.jpg"
+  },
+  {
+    "revision": "dcfcf3b77210fe85b0abc8260e6fa70e",
+    "url": "/static/media/team-2-800x800.dcfcf3b7.jpg"
+  },
+  {
+    "revision": "5a0e395b3b76565583339619ca3110f4",
+    "url": "/static/media/team-3-800x800.5a0e395b.jpg"
+  },
+  {
+    "revision": "7d13384ce79a46a09cd5871cf5156544",
+    "url": "/static/media/team-4-470x470.7d13384c.png"
+  },
+  {
+    "revision": "9378e368a757bd0c7ca556a0258cb527",
+    "url": "/static/media/vue.9378e368.jpg"
+  }
+]);
