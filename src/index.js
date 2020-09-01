f9be8812956ac7a2275b79886820fe9ac1b6f512
EINazare <emanuelioannazare@gmail.com>
2020-09-01 10:56:20 +0300

    feature: add routes for login and register

--- src/index.js
+++ src/index.js
@@ -5,12 +5,16 @@ import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
 import Dashboard from "views/Dashboard.js";
+import Login from "views/Login.js";
+import Register from "views/Register.js";
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-      <Route path="/dashboard" component={Dashboard} />
-      <Redirect from="/" to="/dashboard" />
+      <Route path="/admin/dashboard" component={Dashboard} />
+      <Route path="/auth/login" component={Login} />
+      <Route path="/auth/register" component={Register} />
+      <Redirect from="/" to="/admin/dashboard" />
     </Switch>
   </BrowserRouter>,
   document.getElementById("root")
