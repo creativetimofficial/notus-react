8e15edb24cfdc73e26b2e5124ccdb694478d55ce
EINazare <emanuelioannazare@gmail.com>
2020-09-01 15:33:05 +0300

    fix: solve path imports

--- src/views/Profile.js
+++ src/views/Profile.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-import Navbar from "components/Navbar.js";
-import Footer from "components/Footer.js";
+import Navbar from "components/Navbars/AuthNavbar.js";
+import Footer from "components/Footers/Footer.js";
 
 export default function Profile() {
   return (
