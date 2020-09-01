dd29a4822f4368cf22c129085f9a6ee61322511f
EINazare <emanuelioannazare@gmail.com>
2020-09-01 17:09:51 +0300

    feature: add tailwind compiler

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import "assets/styles/tailwind.css";
 
 // layouts
 
