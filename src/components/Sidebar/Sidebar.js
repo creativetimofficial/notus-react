99b9b9f030730d1aee4f5553ea2251d9e3ddc859
EINazare <emanuelioannazare@gmail.com>
2020-09-01 15:38:26 +0300

    style: run prettier

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -133,7 +133,6 @@ export default function Sidebar() {
             {/* Navigation */}
 
             <ul className="md:flex-col md:min-w-full flex flex-col list-none md:mb-4">
-
               <li className="items-center">
                 <Link
                   className="text-gray-800 hover:text-gray-600 text-xs uppercase py-3 font-bold block"
