ebd32be2ed76157e24d51b83daffb65093b3ef04
EINazare <emanuelioannazare@gmail.com>
2020-09-01 15:38:48 +0300

    feature: add admin layout

---
+++ src/layouts/Admin.js
@@ -0,0 +1,90 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+// components
+
+import AdminNavbar from "components/Navbars/AdminNavbar.js";
+import Sidebar from "components/Sidebar/Sidebar.js";
+import HeaderStats from "components/Headers/HeaderStats.js";
+
+// views
+
+import Dashboard from "views/admin/Dashboard.js";
+import Maps from "views/admin/Maps.js";
+import Settings from "views/admin/Settings.js";
+import Tables from "views/admin/Tables.js";
+
+export default function Admin() {
+  return (
+    <>
+      <Sidebar />
+      <div className="relative md:ml-64 bg-gray-200">
+        <AdminNavbar />
+        {/* Header */}
+        <HeaderStats />
+        <div className="px-4 md:px-10 mx-auto w-full -m-24">
+          <Switch>
+            <Route path="/admin/dashboard" component={Dashboard} />
+            <Route path="/admin/maps" component={Maps} />
+            <Route path="/admin/settings" component={Settings} />
+            <Route path="/admin/tables" component={Tables} />
+          </Switch>
+          <footer className="block py-4">
+            <div className="container mx-auto px-4">
+              <hr className="mb-4 border-b-1 border-gray-300" />
+              <div className="flex flex-wrap items-center md:justify-between justify-center">
+                <div className="w-full md:w-4/12 px-4">
+                  <div className="text-sm text-gray-600 font-semibold py-1">
+                    Copyright © {new Date().getFullYear()}{" "}
+                    <a
+                      href="https://www.creative-tim.com"
+                      className="text-gray-600 hover:text-gray-800 text-sm font-semibold py-1"
+                    >
+                      Creative Tim
+                    </a>
+                  </div>
+                </div>
+                <div className="w-full md:w-8/12 px-4">
+                  <ul className="flex flex-wrap list-none md:justify-end  justify-center">
+                    <li>
+                      <a
+                        href="https://www.creative-tim.com"
+                        className="text-gray-700 hover:text-gray-900 text-sm font-semibold block py-1 px-3"
+                      >
+                        Creative Tim
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="https://www.creative-tim.com/presentation"
+                        className="text-gray-700 hover:text-gray-900 text-sm font-semibold block py-1 px-3"
+                      >
+                        About Us
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="http://blog.creative-tim.com"
+                        className="text-gray-700 hover:text-gray-900 text-sm font-semibold block py-1 px-3"
+                      >
+                        Blog
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="https://github.com/creativetimofficial/tailwind-starter-kit/blob/master/LICENSE.md"
+                        className="text-gray-700 hover:text-gray-900 text-sm font-semibold block py-1 px-3"
+                      >
+                        MIT License
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </footer>
+        </div>
+      </div>
+    </>
+  );
+}
