38568327e30fb48286d079267b49716a5b9f9800
EINazare <emanuelioannazare@gmail.com>
2020-09-01 16:20:55 +0300

    feature: add settings page

---
+++ src/views/admin/Settings.js
@@ -0,0 +1,274 @@
+import React from "react";
+
+// components
+
+export default function Settings() {
+  return (
+    <>
+      <div className="flex flex-wrap">
+        <div className="w-full lg:w-8/12 px-4">
+          <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-gray-200 border-0">
+            <div className="rounded-t bg-white mb-0 px-6 py-6">
+              <div className="text-center flex justify-between">
+                <h6 className="text-gray-800 text-xl font-bold">My account</h6>
+                <button
+                  className="bg-blue-500 text-white active:bg-blue-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1"
+                  type="button"
+                  style={{ transition: "all .15s ease" }}
+                >
+                  Settings
+                </button>
+              </div>
+            </div>
+            <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
+              <form>
+                <h6 className="text-gray-500 text-sm mt-3 mb-6 font-bold uppercase">
+                  User Information
+                </h6>
+                <div className="flex flex-wrap">
+                  <div className="w-full lg:w-6/12 px-4">
+                    <div className="relative w-full mb-3">
+                      <label
+                        className="block uppercase text-gray-700 text-xs font-bold mb-2"
+                        htmlFor="grid-password"
+                      >
+                        Username
+                      </label>
+                      <input
+                        type="text"
+                        className="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full"
+                        defaultValue="lucky.jesse"
+                        style={{ transition: "all .15s ease" }}
+                      />
+                    </div>
+                  </div>
+                  <div className="w-full lg:w-6/12 px-4">
+                    <div className="relative w-full mb-3">
+                      <label
+                        className="block uppercase text-gray-700 text-xs font-bold mb-2"
+                        htmlFor="grid-password"
+                      >
+                        Email address
+                      </label>
+                      <input
+                        type="email"
+                        className="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full"
+                        defaultValue="jesse@example.com"
+                        style={{ transition: "all .15s ease" }}
+                      />
+                    </div>
+                  </div>
+                  <div className="w-full lg:w-6/12 px-4">
+                    <div className="relative w-full mb-3">
+                      <label
+                        className="block uppercase text-gray-700 text-xs font-bold mb-2"
+                        htmlFor="grid-password"
+                      >
+                        First Name
+                      </label>
+                      <input
+                        type="text"
+                        className="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full"
+                        defaultValue="Lucky"
+                        style={{ transition: "all .15s ease" }}
+                      />
+                    </div>
+                  </div>
+                  <div className="w-full lg:w-6/12 px-4">
+                    <div className="relative w-full mb-3">
+                      <label
+                        className="block uppercase text-gray-700 text-xs font-bold mb-2"
+                        htmlFor="grid-password"
+                      >
+                        Last Name
+                      </label>
+                      <input
+                        type="text"
+                        className="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full"
+                        defaultValue="Jesse"
+                        style={{ transition: "all .15s ease" }}
+                      />
+                    </div>
+                  </div>
+                </div>
+
+                <hr className="mt-6 border-b-1 border-gray-400" />
+
+                <h6 className="text-gray-500 text-sm mt-3 mb-6 font-bold uppercase">
+                  Contact Information
+                </h6>
+                <div className="flex flex-wrap">
+                  <div className="w-full lg:w-12/12 px-4">
+                    <div className="relative w-full mb-3">
+                      <label
+                        className="block uppercase text-gray-700 text-xs font-bold mb-2"
+                        htmlFor="grid-password"
+                      >
+                        Address
+                      </label>
+                      <input
+                        type="text"
+                        className="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full"
+                        defaultValue="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09"
+                        style={{ transition: "all .15s ease" }}
+                      />
+                    </div>
+                  </div>
+                  <div className="w-full lg:w-4/12 px-4">
+                    <div className="relative w-full mb-3">
+                      <label
+                        className="block uppercase text-gray-700 text-xs font-bold mb-2"
+                        htmlFor="grid-password"
+                      >
+                        City
+                      </label>
+                      <input
+                        type="email"
+                        className="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full"
+                        defaultValue="New York"
+                        style={{ transition: "all .15s ease" }}
+                      />
+                    </div>
+                  </div>
+                  <div className="w-full lg:w-4/12 px-4">
+                    <div className="relative w-full mb-3">
+                      <label
+                        className="block uppercase text-gray-700 text-xs font-bold mb-2"
+                        htmlFor="grid-password"
+                      >
+                        Country
+                      </label>
+                      <input
+                        type="text"
+                        className="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full"
+                        defaultValue="United States"
+                        style={{ transition: "all .15s ease" }}
+                      />
+                    </div>
+                  </div>
+                  <div className="w-full lg:w-4/12 px-4">
+                    <div className="relative w-full mb-3">
+                      <label
+                        className="block uppercase text-gray-700 text-xs font-bold mb-2"
+                        htmlFor="grid-password"
+                      >
+                        Postal Code
+                      </label>
+                      <input
+                        type="text"
+                        className="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full"
+                        defaultValue="Postal Code"
+                        style={{ transition: "all .15s ease" }}
+                      />
+                    </div>
+                  </div>
+                </div>
+
+                <hr className="mt-6 border-b-1 border-gray-400" />
+
+                <h6 className="text-gray-500 text-sm mt-3 mb-6 font-bold uppercase">
+                  About Me
+                </h6>
+                <div className="flex flex-wrap">
+                  <div className="w-full lg:w-12/12 px-4">
+                    <div className="relative w-full mb-3">
+                      <label
+                        className="block uppercase text-gray-700 text-xs font-bold mb-2"
+                        htmlFor="grid-password"
+                      >
+                        About me
+                      </label>
+                      <textarea
+                        type="text"
+                        className="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full"
+                        rows="4"
+                        style={{ transition: "all .15s ease" }}
+                      >A beautiful Dashboard for React & Tailwindcss. It is Free and Open Source.</textarea>
+                    </div>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+        <div className="w-full lg:w-4/12 px-4">
+          <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg mt-16">
+            <div className="px-6">
+              <div className="flex flex-wrap justify-center">
+                <div className="w-full px-4 flex justify-center">
+                  <div className="relative">
+                    <img
+                      alt="..."
+                      src={require("assets/img/team-2-800x800.jpg")}
+                      className="shadow-xl rounded-full h-auto align-middle border-none absolute -m-16 -ml-20 lg:-ml-16"
+                      style={{ maxWidth: "150px" }}
+                    />
+                  </div>
+                </div>
+                <div className="w-full px-4 text-center mt-20">
+                  <div className="flex justify-center py-4 lg:pt-4 pt-8">
+                    <div className="mr-4 p-3 text-center">
+                      <span className="text-xl font-bold block uppercase tracking-wide text-gray-700">
+                        22
+                      </span>
+                      <span className="text-sm text-gray-500">Friends</span>
+                    </div>
+                    <div className="mr-4 p-3 text-center">
+                      <span className="text-xl font-bold block uppercase tracking-wide text-gray-700">
+                        10
+                      </span>
+                      <span className="text-sm text-gray-500">Photos</span>
+                    </div>
+                    <div className="lg:mr-4 p-3 text-center">
+                      <span className="text-xl font-bold block uppercase tracking-wide text-gray-700">
+                        89
+                      </span>
+                      <span className="text-sm text-gray-500">Comments</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="text-center mt-12">
+                <h3 className="text-xl font-semibold leading-normal mb-2 text-gray-800 mb-2">
+                  Jenna Stones
+                </h3>
+                <div className="text-sm leading-normal mt-0 mb-2 text-gray-500 font-bold uppercase">
+                  <i className="fas fa-map-marker-alt mr-2 text-lg text-gray-500"></i>{" "}
+                  Los Angeles, California
+                </div>
+                <div className="mb-2 text-gray-700 mt-10">
+                  <i className="fas fa-briefcase mr-2 text-lg text-gray-500"></i>
+                  Solution Manager - Creative Tim Officer
+                </div>
+                <div className="mb-2 text-gray-700">
+                  <i className="fas fa-university mr-2 text-lg text-gray-500"></i>
+                  University of Computer Science
+                </div>
+              </div>
+              <div className="mt-10 py-10 border-t border-gray-300 text-center">
+                <div className="flex flex-wrap justify-center">
+                  <div className="w-full lg:w-9/12 px-4">
+                    <p className="mb-4 text-lg leading-relaxed text-gray-800">
+                      An artist of considerable range, Jenna the name taken by
+                      Melbourne-raised, Brooklyn-based Nick Murphy writes,
+                      performs and records all of his own music, giving it a
+                      warm, intimate feel with a solid groove structure. An
+                      artist of considerable range.
+                    </p>
+                    <a
+                      href="#pablo"
+                      className="font-normal text-pink-500"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      Show more
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
