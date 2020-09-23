66d93bf30a949a76ed8e173f0a5007c34a7e11f1
EINazare <emanuelioannazare@gmail.com>
2020-09-23 16:34:15 +0300

    docs: change links and names

--- src/components/Cards/CardSettings.js
+++ src/components/Cards/CardSettings.js
@@ -172,7 +172,7 @@ export default function CardSettings() {
                     className="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150"
                     rows="4"
                   >
-                    A beautiful Dashboard for React & Tailwindcss. It is Free
+                    A beautiful Dashboard for React & Tailwind CSS. It is Free
                     and Open Source.
                   </textarea>
                 </div>
