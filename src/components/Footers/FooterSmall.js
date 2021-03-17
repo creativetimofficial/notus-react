61026facad4b9c7759944c976dd84a310b9379b8
EINazare <emanuelioannazare@gmail.com>
2021-03-17 13:38:59 +0200

    docs: change master to main

--- src/components/Footers/FooterSmall.js
+++ src/components/Footers/FooterSmall.js
@@ -52,7 +52,7 @@ export default function FooterSmall(props) {
                 </li>
                 <li>
                   <a
-                    href="https://github.com/creativetimofficial/notus-react/blob/master/LICENSE.md?ref=nr-footer-small"
+                    href="https://github.com/creativetimofficial/notus-react/blob/main/LICENSE.md?ref=nr-footer-small"
                     className="text-white hover:text-gray-400 text-sm font-semibold block py-1 px-3"
                   >
                     MIT License
