b549f4b80f6a7a393876ffb37f52bc12df1611c5
EINazare <emanuelioannazare@gmail.com>
2021-03-18 13:47:33 +0200

    fix: solve typo

--- src/components/Dropdowns/IndexDropdown.js
+++ src/components/Dropdowns/IndexDropdown.js
@@ -99,7 +99,7 @@ const IndexDropdown = () => {
           to="/landing"
           className="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-coolGray-700"
         >
-          Lading
+          Landing
         </Link>
         <Link
           to="/profile"
