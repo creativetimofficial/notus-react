3aca7730023851ddbd78ef2f84bbaccebf2f0e4f
EINazare <emanuelioannazare@gmail.com>
2021-03-18 18:27:33 +0200

    fix: solve broken colors

--- src/views/Index.js
+++ src/views/Index.js
@@ -346,7 +346,7 @@ export default function Index() {
                     href="https://www.creative-tim.com/learning-lab/tailwind/vue/alerts/notus?ref=vtw-index"
                     target="_blank"
                   >
-                    <div className="bg-green-500 shadow-lg rounded-lg text-center p-8 mt-8">
+                    <div className="bg-emerald-500 shadow-lg rounded-lg text-center p-8 mt-8">
                       <img
                         alt="..."
                         className="shadow-md rounded-full max-w-full w-16 mx-auto p-2 bg-white"
--- src/views/Landing.js
+++ src/views/Landing.js
@@ -97,7 +97,7 @@ export default function Landing() {
               <div className="pt-6 w-full md:w-4/12 px-4 text-center">
                 <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
                   <div className="px-4 py-5 flex-auto">
-                    <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-green-400">
+                    <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-emerald-400">
                       <i className="fas fa-fingerprint"></i>
                     </div>
                     <h6 className="text-xl font-semibold">Verified Company</h6>
