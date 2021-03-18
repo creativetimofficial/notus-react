89ab87a815a516012044938c50e4555dc64560f0
EINazare <emanuelioannazare@gmail.com>
2021-03-18 18:11:43 +0200

    fix: solve broken colors

--- src/views/Profile.js
+++ src/views/Profile.js
@@ -58,7 +58,7 @@ export default function Profile() {
                   <div className="w-full lg:w-4/12 px-4 lg:order-3 lg:text-right lg:self-center">
                     <div className="py-6 px-3 mt-32 sm:mt-0">
                       <button
-                        className="bg-blue-500 active:bg-blue-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150"
+                        className="bg-lightBlue-500 active:bg-lightBlue-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150"
                         type="button"
                       >
                         Connect
@@ -123,7 +123,7 @@ export default function Profile() {
                       </p>
                       <a
                         href="#pablo"
-                        className="font-normal text-blue-500"
+                        className="font-normal text-lightBlue-500"
                         onClick={(e) => e.preventDefault()}
                       >
                         Show more
