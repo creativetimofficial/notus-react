73f4f37a86d40f2627c30a46511cb85884ded586
hmayenve <henry@amida.com>
2021-04-22 08:50:21 -0400

    Displaying data from pyspark

--- src/components/Misc/Circle.js
+++ src/components/Misc/Circle.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Circle({ number}){
   let designation = "";
-  if (number && number <= 2) {
+  if (number && number <= 2 || number == 0) {
     designation = "circle-bad";
   } else if (number > 2 && number < 4) {
     designation = "circle-okay";
@@ -10,7 +10,7 @@ export default function Circle({ number}){
     designation = "circle-good";
   }
   return (
-    <div className={number ? 'fade-in' : 'pre-fade'}>
+    <div className={number || number == 0 ? 'fade-in' : 'pre-fade'}>
       <div className={"inline-block text-center circle " + designation}>
         {number}
       </div>
