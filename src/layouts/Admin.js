b5095f180ac1542a94c55f46a8401b6dba559453
hmayenve <henry@amida.com>
2021-06-09 13:58:50 -0400

    empty db bug fix

--- src/layouts/Admin.js
+++ src/layouts/Admin.js
@@ -20,52 +20,52 @@ export default function Admin() {
 
   useEffect(() => {
     fetch(`${process.env.REACT_APP_HEDIS_MEASURE_API_URL}measures`)
-      .then(response => response.json())
-      .then(res => {
-        const specialName = "Composite";
-        const first = res.find(a => a.name === specialName);
-        const theRestSorted = res.filter(a => a.name !== specialName).sort((a, b) => a.name.localeCompare(b.name));
-        setMeasures([first, ...theRestSorted])
-    })
+      .then((response) => response.json())
+      .then((res) => {
+        if (res && res.length > 0) {
+          const specialName = "Composite";
+          const first = res.find((a) => a.name === specialName);
+          const theRestSorted = res
+            .filter((a) => a.name !== specialName)
+            .sort((a, b) => a.name.localeCompare(b.name));
+          setMeasures([first, ...theRestSorted]);
+        }
+      });
   }, []);
 
   return (
     <>
       {/* <Sidebar /> */}
       {/* <div className="relative md:ml-64 bg-gray-200"> */}
-        <AdminNavbar />
-        {/* Header */}
-        <div className="bg-blue-600 pb-32 pt-12">
-        </div> 
-        <div className="px-4 md:px-10 mx-auto w-full -m-24">
-          <Switch>
-            <Route
-              path='/dashboard'
-              render={(props) => (
-                <Dashboard {...props} measures={measures} />
-              )}
-            />
-            <Route path="/dashboard" exact component={Dashboard} />
-            <Route path="/maps" exact component={Maps} />
-            <Route path="/settings" exact component={Settings} />
-            <Route path="/tables" exact component={Tables} />
-            {/*<Redirect from="/admin" to="/admin/dashboard" />*/}
-            {/*<Redirect from="/" to="/admin/dashboard" />*/}
-            {/* temp fix - figure out later*/}
-            <Route
-              path="/measures/:measureName" 
-              render={(props) => (
-                <Measure {...props} measures={measures} />
-              )}/>
-            <Route
-              path="/"
-              render={(props) => (
-                <MeasureDashboard {...props} measures={measures} />
-              )}
-              />
-          </Switch>
-          <FooterAdmin />
-        </div>
+      <AdminNavbar />
+      {/* Header */}
+      <div className="bg-blue-600 pb-32 pt-12"></div>
+      <div className="px-4 md:px-10 mx-auto w-full -m-24">
+        <Switch>
+          <Route
+            path="/dashboard"
+            render={(props) => <Dashboard {...props} measures={measures} />}
+          />
+          <Route path="/dashboard" exact component={Dashboard} />
+          <Route path="/maps" exact component={Maps} />
+          <Route path="/settings" exact component={Settings} />
+          <Route path="/tables" exact component={Tables} />
+          {/*<Redirect from="/admin" to="/admin/dashboard" />*/}
+          {/*<Redirect from="/" to="/admin/dashboard" />*/}
+          {/* temp fix - figure out later*/}
+          <Route
+            path="/measures/:measureName"
+            render={(props) => <Measure {...props} measures={measures} />}
+          />
+          <Route
+            path="/"
+            render={(props) => (
+              <MeasureDashboard {...props} measures={measures} />
+            )}
+          />
+        </Switch>
+        <FooterAdmin />
+      </div>
       {/* </div> */}
     </>
   );
