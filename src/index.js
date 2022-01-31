01255534f20e726552fc3e4c3fe234c74c3b1639
Eric <eawest2@gmail.com>
2022-01-31 11:02:05 -0500

    removed tailwinds, added mui. Checked components to make sure they worked. Edited Package.json so it reflects our porject, not a template.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import "assets/styles/tailwind.css";
 
 // layouts
 
