96b4eb9d4ff0ec5ed3ecfa10773600802d43b083
jmfadeley <jmfadeley@gmail.com>
2022-02-22 22:58:40 -0500

    More small fixes.

--- src/components/D3Container/D3Filter.js
+++ src/components/D3Container/D3Filter.js
@@ -1,54 +1,55 @@
-import { Checkbox, Divider, FormGroup, Grid, Typography } from '@mui/material';
+import {
+  Checkbox, Divider, FormGroup, Grid, Typography,
+} from '@mui/material';
 import { Box } from '@mui/system';
 import React from 'react';
 
 function D3Filter(props) {
+  const { filter } = props;
+  const onChange = props.changeFunction;
 
-    const filter = props.filter;
-    const onChange = props.changeFunction;
-
-    return (
-        <Box>
-            <Divider color='black'/>
-            <Grid container justifyContent='space-evenly' direction="row" alignItems="center" spacing={2} sx={{ width: '100%', p: '3px', m: '2px' }}>
-                <Grid item xs={1}>
-                    <Typography variant='caption'>
-                        {filter.name}
-                    </Typography>
-                </Grid>
-                <Grid item xs={2}>
-                    <Typography variant='caption'>
-                        {filter.included}
-                    </Typography>
-                </Grid>
-                <Grid item xs={2}>
-                    <Typography variant='caption'>
-                        {filter.eligible}
-                    </Typography>
-                </Grid>
-                <Grid item xs={2}>
-                    <Typography variant='caption'>
-                        {filter.numerator}
-                    </Typography>
-                </Grid>
-                <Grid item xs={2}>
-                    <Typography variant='caption'>
-                        {filter.denominator}
-                    </Typography>
-                </Grid>
-                <Grid item xs={2}>
-                    <Typography variant='caption'>
-                        {filter.exclusions}
-                    </Typography>
-                </Grid>
-                <Grid item xs={1}>
-                    <FormGroup sx={{ ml: "8px" }}>
-                        <Checkbox disableRipple defaultChecked size="medium" color="blue" onChange={() => onChange(filter)} />
-                    </FormGroup>
-                </Grid>
-            </Grid >
-        </Box>
-    )
+  return (
+    <Box>
+      <Divider color="black" />
+      <Grid container justifyContent="space-evenly" direction="row" alignItems="center" spacing={2} sx={{ width: '100%', p: '3px', m: '2px' }}>
+        <Grid item xs={1}>
+          <Typography variant="caption">
+            {filter.name}
+          </Typography>
+        </Grid>
+        <Grid item xs={2}>
+          <Typography variant="caption">
+            {filter.included}
+          </Typography>
+        </Grid>
+        <Grid item xs={2}>
+          <Typography variant="caption">
+            {filter.eligible}
+          </Typography>
+        </Grid>
+        <Grid item xs={2}>
+          <Typography variant="caption">
+            {filter.numerator}
+          </Typography>
+        </Grid>
+        <Grid item xs={2}>
+          <Typography variant="caption">
+            {filter.denominator}
+          </Typography>
+        </Grid>
+        <Grid item xs={2}>
+          <Typography variant="caption">
+            {filter.exclusions}
+          </Typography>
+        </Grid>
+        <Grid item xs={1}>
+          <FormGroup sx={{ ml: '8px' }}>
+            <Checkbox disableRipple defaultChecked size="medium" color="blue" onChange={() => onChange(filter)} />
+          </FormGroup>
+        </Grid>
+      </Grid>
+    </Box>
+  )
 }
 
 export default D3Filter;
--- src/components/D3Container/DemoData.js
+++ src/components/D3Container/DemoData.js
@@ -1,122 +1,124 @@
 const date1 = {
-    measure: 'COL',
-    date: "2022-12-14",
-    value: 4.2,
+  measure: 'COL',
+  date: '2022-12-14',
+  value: 4.2,
 };
 
 const date2 = {
-    measure: 'COL',
-    date: "2022-12-15",
-    value: 4.8,
+  measure: 'COL',
+  date: '2022-12-15',
+  value: 4.8,
 };
 
 const date3 = {
-    measure: 'COL',
-    date: "2022-12-16",
-    value: 4.1,
+  measure: 'COL',
+  date: '2022-12-16',
+  value: 4.1,
 };
 
 const date4 = {
-    measure: 'COL',
-    date: "2022-12-17",
-    value: 3.7,
+  measure: 'COL',
+  date: '2022-12-17',
+  value: 3.7,
 };
 
 const date5 = {
-    measure: 'COL',
-    date: "2022-12-18",
-    value: 4.6,
+  measure: 'COL',
+  date: '2022-12-18',
+  value: 4.6,
 };
 
 const date6 = {
-    measure: 'COL',
-    date: "2022-12-19",
-    value: 4.2,
+  measure: 'COL',
+  date: '2022-12-19',
+  value: 4.2,
 };
 
 const date7 = {
-    measure: 'COL',
-    date: "2022-12-20",
-    value: 2.9,
+  measure: 'COL',
+  date: '2022-12-20',
+  value: 2.9,
 };
 
 const date8 = {
-    measure: 'COL',
-    date: "2022-12-21",
-    value: 3.2,
+  measure: 'COL',
+  date: '2022-12-21',
+  value: 3.2,
 };
 
 const date9 = {
-    measure: 'COL',
-    date: "2022-12-22",
-    value: 3.6,
+  measure: 'COL',
+  date: '2022-12-22',
+  value: 3.6,
 };
 
 const date10 = {
-    measure: 'COL',
-    date: "2022-12-23",
-    value: 4.4,
+  measure: 'COL',
+  date: '2022-12-23',
+  value: 4.4,
 };
 
 const date11 = {
-    measure: 'ACP',
-    date: "2022-12-14",
-    value: 3.7,
+  measure: 'ACP',
+  date: '2022-12-14',
+  value: 3.7,
 };
 
 const date12 = {
-    measure: 'ACP',
-    date: "2022-12-15",
-    value: 4.1,
+  measure: 'ACP',
+  date: '2022-12-15',
+  value: 4.1,
 };
 
 const date13 = {
-    measure: 'ACP',
-    date: "2022-12-16",
-    value: 4.9,
+  measure: 'ACP',
+  date: '2022-12-16',
+  value: 4.9,
 };
 
 const date14 = {
-    measure: 'ACP',
-    date: "2022-12-17",
-    value: 4.1,
+  measure: 'ACP',
+  date: '2022-12-17',
+  value: 4.1,
 };
 
 const date15 = {
-    measure: 'ACP',
-    date: "2022-12-18",
-    value: 4.3,
+  measure: 'ACP',
+  date: '2022-12-18',
+  value: 4.3,
 };
 
 const date16 = {
-    measure: 'ACP',
-    date: "2022-12-19",
-    value: 3.8,
+  measure: 'ACP',
+  date: '2022-12-19',
+  value: 3.8,
 };
 
 const date17 = {
-    measure: 'ACP',
-    date: "2022-12-20",
-    value: 4.1,
+  measure: 'ACP',
+  date: '2022-12-20',
+  value: 4.1,
 };
 
 const date18 = {
-    measure: 'ACP',
-    date: "2022-12-21",
-    value: 4.6,
+  measure: 'ACP',
+  date: '2022-12-21',
+  value: 4.6,
 };
 
 const date19 = {
-    measure: 'ACP',
-    date: "2022-12-22",
-    value: 4.2,
+  measure: 'ACP',
+  date: '2022-12-22',
+  value: 4.2,
 };
 
 const date20 = {
-    measure: 'ACP',
-    date: "2022-12-23",
-    value: 5.0,
+  measure: 'ACP',
+  date: '2022-12-23',
+  value: 5.0,
 };
 
-export const dataList = [date1, date2, date3, date4, date5, date6, date7, date8, date9, date10,
-    date11, date12, date13, date14, date15, date16, date17, date18, date19, date20];
+const dataList = [date1, date2, date3, date4, date5, date6, date7, date8, date9, date10,
+  date11, date12, date13, date14, date15, date16, date17, date18, date19, date20];
+
+export default dataList;
