636de04b2db7cc3cf96812ac996003d7c510eefc
jmfadeley <jmfadeley@gmail.com>
2022-02-24 16:31:35 -0500

    [SAR-367] Preprocessor added.

--- src/notFound.js
+++ src/notFound.js
@@ -3,8 +3,8 @@ import React from 'react';
 class NotFound extends React.PureComponent {
   render() {
     return (
-      <div style={{ textAlign: 'center' }}>
-        <h1 style={{ fontSize: '200px' }}>404 Error</h1>
+      <div className="not-found">
+        <h1>404 Error</h1>
       </div>
     );
   }
