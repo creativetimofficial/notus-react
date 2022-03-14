aacbf72fad018e52c3198b259e0f8c84812b02bd
jmfadeley <jmfadeley@gmail.com>
2022-03-14 12:29:24 -0400

    That should do it.

--- src/components/Common/TabPanel.js
+++ src/components/Common/TabPanel.js
@@ -2,18 +2,21 @@ import { Box } from '@mui/system';
 import React from 'react';
 import PropTypes from 'prop-types';
 
+// Mui's own documentation uses div over Box. Recommend leaving it.
+// https://mui.com/components/tabs/
 function TabPanel({
   children, value, index,
 }) {
   return (
     <div
+      className="tab-panel"
       role="tabpanel"
       hidden={value !== index}
       id={`tabpanel-${index}`}
       aria-labelledby={`tab-${index}`}
     >
       {value === index && (
-        <Box sx={{ p: 3 }}>
+        <Box className="tab-panel__box">
           {children}
         </Box>
       )}
