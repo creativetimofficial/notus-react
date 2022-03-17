26affb60064165bab77d39513d3052ab61073575
jmfadeley <jmfadeley@gmail.com>
2022-03-17 17:17:54 -0400

    Slow going but really getting there.

--- src/components/FilterMenu/FilterDrawer.js
+++ src/components/FilterMenu/FilterDrawer.js
@@ -45,32 +45,28 @@ function FilterDrawer({
             <Typography className="filter-drawer__title" variant="h6">Filters</Typography>
           </Grid>
           <Grid item className="filter-drawer__close-icon-panel">
-            <CloseIcon className="filter-drawer__close-icon" color="black" onClick={toggleDrawer(false)} />
+            <CloseIcon className="filter-drawer__close-icon" onClick={toggleDrawer(false)} />
           </Grid>
         </Grid>
-        <Grid container className="filter-drawer__items-panel" direction="column">
-          <Grid container item className="filter-drawer__options-panel" direction="column" sx={{ mt: '10px' }}>
-            <FilterDrawerItem filterItem={filterDrawerItemData.domainsOfCare} />
-            <Grid container item className="filter-drawer__slider-panel" sx={{ mt: '10px' }} spacing={1} direction="row">
-              <Grid item className="filter-drawer__slider-title">
-                <Typography color="black.dark" variant="body1">Percent Range:</Typography>
-              </Grid>
-              <Grid item className="filter-drawer__help-panel">
-                <HelpIcon className="filter-drawer__help" />
-              </Grid>
+        <Grid container item className="filter-drawer__options-panel">
+          <FilterDrawerItem filterItem={filterDrawerItemData.domainsOfCare} />
+          <Grid container item className="filter-drawer__slider-panel">
+            <Grid item className="filter-drawer__slider-title">
+              <Typography className="filter-drawer__slider-label" variant="body1">Percent Range:</Typography>
             </Grid>
-            <Grid item className="filter-drawer__slider-panel">
-              <Box sx={{ minwidth: '150px', ml: '10px' }}>
-                <Slider
-                  getAriaLabel={() => 'Temperature range'}
-                  value={percentSliderValue}
-                  onChange={handleSliderChange}
-                  valueLabelDisplay="auto"
-                  getAriaValueText={sliderValuetext}
-                  sx={{ color: 'blue.dark', width: '80%' }}
-                />
-              </Box>
+            <Grid item className="filter-drawer__help-panel">
+              <HelpIcon className="filter-drawer__help" />
             </Grid>
+          </Grid>
+          <Grid item className="filter-drawer__slider-body">
+            <Slider
+              getAriaLabel={() => 'Temperature range'}
+              value={percentSliderValue}
+              onChange={handleSliderChange}
+              valueLabelDisplay="auto"
+              getAriaValueText={sliderValuetext}
+              className="filter-drawer__slider"
+            />
           </Grid>
           <FilterDrawerItem filterItem={filterDrawerItemData.starRating} />
           <Grid className="filter-drawer__button-control-panel" sx={{ mt: '30px' }} container justifyContent="space-evenly" alignItems="center" direction="row">
