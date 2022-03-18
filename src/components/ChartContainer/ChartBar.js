3aa2c537d807ecf4b3ef6da08e70330b1ac976f4
Keith <keithjayjones@gmail.com>
2022-03-18 16:17:08 -0400

    Brought back the footer
    Converted old design style to new style

--- src/components/ChartContainer/ChartBar.js
+++ src/components/ChartContainer/ChartBar.js
@@ -16,8 +16,6 @@ function ChartBar({ filterDrawerOpen, toggleFilterDrawer }) {
     toggleFilterDrawer(!filterDrawerOpen);
   };
 
-  const displayMvp = (`${process.env.REACT_APP_MVP_SETTING}` === 'true') ? 'none' : 'block';
-
   return (
     <Box>
       <Grid container direction="row" justifyContent="flex-end" spacing={0.1}>
@@ -31,22 +29,28 @@ function ChartBar({ filterDrawerOpen, toggleFilterDrawer }) {
           </Button>
         </Grid>
         <Grid item sx={buttonStyling}>
-          <Box sx={{ display: displayMvp }}>
-            <Button disabled key="d3-options" color="black" variant="text" startIcon={<TuneIcon />}>
-              <Typography variant="caption">
-                Options
-              </Typography>
-            </Button>
-          </Box>
+          {
+            process.env.REACT_APP_MVP_SETTING === 'false'
+              && (
+                <Button disabled key="d3-options" color="black" variant="text" startIcon={<TuneIcon />}>
+                  <Typography variant="caption">
+                    Options
+                  </Typography>
+                </Button>
+              )
+            }
         </Grid>
         <Grid item sx={buttonStyling}>
-          <Box sx={{ display: displayMvp }}>
-            <Button disabled key="d3-export" color="black" variant="text" startIcon={<SimCardDownloadIcon />}>
-              <Typography variant="caption">
-                Export
-              </Typography>
-            </Button>
-          </Box>
+          {
+            process.env.REACT_APP_MVP_SETTING === 'false'
+            && (
+              <Button disabled key="d3-export" color="black" variant="text" startIcon={<SimCardDownloadIcon />}>
+                <Typography variant="caption">
+                  Export
+                </Typography>
+              </Button>
+            )
+          }
         </Grid>
         <Grid item sx={buttonStyling}>
           <Button
