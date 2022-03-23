b50a882db20b0b4d0261cad46eeeefa7480f7fb3
Tim <tim@Amidas-MacBook-Pro.local>
2022-03-23 11:26:56 -0400

    Adding styling to Measure by line chart

--- src/components/ChartContainer/D3IndicatorByLineChart.js
+++ src/components/ChartContainer/D3IndicatorByLineChart.js
@@ -1,8 +1,14 @@
 import * as d3 from 'd3';
 import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
+import { TickChange } from 'components/Utilites/TickChange';
+import { colorMappingProps } from './D3Props';
 
-function D3IndicatorByLineChart({ byLineDisplayData }) {
+function D3IndicatorByLineChart({
+  byLineDisplayData,
+  colorMapping,
+  measureInfo,
+}) {
   // Binder for react to apply changes to the svg
   const D3IndicatorLineChart = useRef();
 
@@ -14,11 +20,11 @@ function D3IndicatorByLineChart({ byLineDisplayData }) {
     top: 50,
     right: 30,
     bottom: 75,
-    left: 30,
+    left: 45,
   };
   const box = document.querySelector('.MuiGrid-item');
-  const widthBase = (window.innerWidth || document.body.clientWidth);
-  const width = box === null ? (widthBase * 0.8) : box.offsetWidth - 200;
+  const widthBase = window.innerWidth || document.body.clientWidth;
+  const width = box === null ? widthBase * 0.8 : box.offsetWidth - 250;
   const height = 500;
   const tickCount = byLineDisplayData.length;
 
@@ -31,35 +37,40 @@ function D3IndicatorByLineChart({ byLineDisplayData }) {
       .select(D3IndicatorLineChart.current)
       .attr('width', width)
       .attr('height', height)
-      .style('background-color', 'white')
-      .style('color', 'black')
+      .attr('class', 'd3-indicator-by-line-chart__line-chart')
       .append('g')
-      .attr('transform', `translate(${margin.left},${margin.top})`);
+      .attr('transform', `translate(${margin.left + 65},${margin.top})`);
 
     // Generates labels and context for x axis
-    const x = d3.scaleTime()
-    // What data we're measuring
-      .domain(d3.extent(byLineDisplayData, (d) => parseDate(d.date.split('T')[0])))
-    // The 'width' of the data
+    const x = d3
+      .scaleTime()
+      // What data we're measuring
+      .domain(
+        d3.extent(byLineDisplayData, (d) => parseDate(d.date.split('T')[0]))
+      )
+      // The 'width' of the data
       .range([0, width]);
 
     // X Axis labels and context
     svg
       .append('g')
-      .attr('transform', `translate(0,${height - margin.bottom})`)
+      .attr('transform', `translate(0,${height - margin.bottom / 1.4})`)
+      .attr('class', 'd3-indicator-by-line-chart__datesX')
       .call(
-        d3.axisBottom(x).ticks(tickCount).tickFormat(d3.timeFormat('%d-%b-%Y')),
+        d3.axisBottom(x).ticks(tickCount).tickFormat(d3.timeFormat('%b %d'))
       );
 
     // Generates Label and context for y axis
     d3.max(byLineDisplayData, (d) => d.value);
-
     const y = d3
       .scaleLinear()
-      .domain([0, 5])
+      .domain([0, 100])
       .range([height - margin.bottom, 0]);
 
-    svg.append('g').call(d3.axisLeft(y));
+    svg
+      .append('g')
+      .attr('class', 'd3-indicator-by-line-chart__ratingY')
+      .call(d3.axisLeft(y));
 
     // Grid
     // gridlines in x axis function
@@ -69,33 +80,85 @@ function D3IndicatorByLineChart({ byLineDisplayData }) {
 
     // gridlines in y axis function
     function makeYGridlines() {
-      return d3.axisLeft(y).ticks(10);
+      return d3.axisLeft(y).ticks(5);
     }
 
     // add the X gridlines
     svg
       .append('g')
-      .attr('class', 'axis-grid')
+      .attr('class', 'd3-indicator-by-line-chart__x-axis')
       .attr('transform', `translate(0,${height})`)
       .call(makeXGridlines().tickSize(-height).tickFormat(''));
 
     // add the Y gridlines
     svg
       .append('g')
-      .attr('class', 'axis-grid')
+      .attr('class', 'd3-indicator-by-line-chart__y-axis')
       .call(makeYGridlines().tickSize(-width).tickFormat(''));
 
     d3.selectAll('.axis-grid line').style('stroke', 'lightgray');
-
+    // Change Ticks to Percent
+    TickChange();
     // Generates the actual line
     const line = d3
       .line()
       // .curve(d3.curveCardinal)
       .x((d) => x(parseDate(d.date.split('T')[0])))
-      .y((d) => y(d.value / 20));
+      .y((d) => y(d.value));
+
+    // X axis label:
+    svg
+      .append('text')
+      .attr('x', width / 2)
+      .attr('y', height + 20)
+      .attr('class', 'd3-indicator-by-line-chart__label')
+      .text('Year to Date');
+
+    // Y axis label:
+    svg
+      .append('text')
+      .attr('class', 'd3-indicator-by-line-chart__label')
+      .attr('transform', 'rotate(-90)')
+      .attr('y', -margin.left - 25)
+      .attr('x', -margin.top - 160)
+      .text('Percent');
+    // Tool Tips
+    const tooltip = d3
+      .select('body')
+      .append('div')
+      .attr('class', 'd3-chart__tooltip');
+
+    const toolTipGenerator = (event) => {
+      // const avg30 = margin.left * 0.3;
+      // const tickWidth = Math.floor(width / tickCount + avg30);
+      // const index = Math.floor((event.offsetX - margin.left) / tickWidth);
+      console.log('byLineDisplayData', byLineDisplayData);
+      const MeasureValue =
+        measureInfo[event.srcElement.__data__[index].measure].displayLabel;
+      // const measureDisplay =
+      //   MeasureValue === 'Composite'
+      //     ? `${MeasureValue} Score`
+      //     : `Measure: ${MeasureValue}`;
+      // const valueDisplay = `Value: ${
+      //   Math.floor(event.srcElement.__data__[index].value * 100) / 100
+      // }%`;
+      // const dateDisplay = TimeFormatter(event.srcElement.__data__[index].date);
+      tooltip.text(`${measureDisplay} \n ${valueDisplay} \n ${dateDisplay}`);
+      // tooltip.text('Hello World');
+      // const { color } = colorMapping.find(
+      //   (mapping) => mapping.measure === event.target.__data__[0].measure
+      // );
+      return (
+        tooltip
+          .attr('data-html', 'true')
+          // .style('background-color', color)
+          .style('visibility', 'visible')
+          .style('top', `${event.pageY - 10}px`)
+          .style('left', `${event.pageX + 10}px`)
+      );
+    };
 
     // Iterates through an array variation.
-
     svg
       .append('path')
       .datum(byLineDisplayData)
@@ -106,10 +169,23 @@ function D3IndicatorByLineChart({ byLineDisplayData }) {
       .attr('d', line)
       .on('mouseover', (event) => {
         d3.select(event.currentTarget).attr('opacity', '1');
+        toolTipGenerator(event);
       })
+      .on('mousemove', (event) => toolTipGenerator(event))
       .on('mouseout', (event) => {
-        d3.select(event.currentTarget).attr('opacity', '.33');
+        d3.select(event.currentTarget).attr('opacity', '.50');
+        return tooltip.style('visibility', 'hidden');
       });
+    // Create line at end of chart to make it a complete box
+    svg
+      .append('line')
+      .attr('x1', width)
+      .attr('y1', 0)
+      .attr('x2', width)
+      .attr('y2', height - margin.bottom)
+      .style('stroke-width', 1)
+      .style('stroke', '#CFD8DC')
+      .style('fill', 'none');
   });
 
   return (
@@ -124,12 +200,18 @@ D3IndicatorByLineChart.propTypes = {
     PropTypes.shape({
       value: PropTypes.number,
       date: PropTypes.string,
-    }),
+    })
   ),
+  measureInfo: PropTypes.shape({
+    displayLabel: PropTypes.string,
+  }),
+  colorMapping: colorMappingProps,
 };
 
 D3IndicatorByLineChart.defaultProps = {
   byLineDisplayData: [],
+  measureInfo: {},
+  colorMapping: [],
 };
 
 export default D3IndicatorByLineChart;
--- src/components/Utilites/TickChange.js
+++ src/components/Utilites/TickChange.js
@@ -2,14 +2,18 @@
 const TickChange = () => {
   const ticksFound = document.querySelectorAll('.tick > text');
   const tickLinesFound = document.querySelectorAll('.tick > line');
-  const tickLinesFoundX = document.querySelectorAll('.d3-chart__datesX > .tick > line');
+  const tickLinesFoundX = document.querySelectorAll(
+    '.d3-chart__datesX > .tick > line'
+  );
   const DomainsFound = document.querySelectorAll('.domain');
-
+  // D3 Chart
+  // D3 Chart
+  // D3 Chart
   for (let i = 0; i < tickLinesFoundX.length; i += 1) {
-    tickLinesFoundX[i].style.display = 'none'
+    tickLinesFoundX[i].style.display = 'none';
   }
   //   Making the X-AXIS DISAPPEAR
-  DomainsFound[0].style.display = 'none'
+  DomainsFound[0].style.display = 'none';
   // Changes Numbers to Percent
   for (let i = 0; i < ticksFound.length; i += 1) {
     const tickInnerHtml = ticksFound[i].innerHTML;
@@ -45,33 +49,42 @@ const TickChange = () => {
   // Removes Ticks on 10,30,50,70 and 90.
   for (let i = 0; i < tickLinesFound.length; i += 1) {
     if (tickLinesFound[i].__data__ === 0) {
-    //   tickLinesFound[i].style.color = 'white'
-      tickLinesFound[i].classList.add('d3-chart__stroke-Dasharray')
+      // tickLinesFound[i].style.color = 'white'
+      tickLinesFound[i].classList.add('d3-chart__stroke-width-opacity');
     } else if (tickLinesFound[i].__data__ === 20) {
-      tickLinesFound[i].classList.add('d3-chart__stroke-Dasharray')
+      tickLinesFound[i].classList.add('d3-chart__stroke-width-opacity');
     } else if (tickLinesFound[i].__data__ === 40) {
-      tickLinesFound[i].classList.add('d3-chart__stroke-Dasharray')
+      tickLinesFound[i].classList.add('d3-chart__stroke-width-opacity');
     } else if (tickLinesFound[i].__data__ === 60) {
-      tickLinesFound[i].classList.add('d3-chart__stroke-Dasharray')
+      tickLinesFound[i].classList.add('d3-chart__stroke-width-opacity');
     } else if (tickLinesFound[i].__data__ === 80) {
-      tickLinesFound[i].classList.add('d3-chart__stroke-Dasharray')
+      tickLinesFound[i].classList.add('d3-chart__stroke-width-opacity');
     } else if (tickLinesFound[i].__data__ === 100) {
-      tickLinesFound[i].classList.add('d3-chart__stroke-Dasharray')
+      tickLinesFound[i].classList.add('d3-chart__stroke-width-opacity');
     }
     // Removes Specific Ticks
     if (tickLinesFound[i].__data__ === 10) {
-      tickLinesFound[i].remove()
+      tickLinesFound[i].remove();
     } else if (tickLinesFound[i].__data__ === 30) {
-      tickLinesFound[i].remove()
+      tickLinesFound[i].remove();
     } else if (tickLinesFound[i].__data__ === 50) {
-      tickLinesFound[i].remove()
+      tickLinesFound[i].remove();
     } else if (tickLinesFound[i].__data__ === 70) {
-      tickLinesFound[i].remove()
+      tickLinesFound[i].remove();
     } else if (tickLinesFound[i].__data__ === 90) {
-      tickLinesFound[i].remove()
+      tickLinesFound[i].remove();
     }
   }
-//   console.log("tickLinesFound",tickLinesFound)
-}
+
+  // D3 Chart Indicator By Line Chart
+  // D3 Chart Indicator By Line Chart
+  // D3 Chart Indicator By Line Chart
+  const tickLinesFoundLineChartX = document.querySelectorAll(
+    '.d3-indicator-by-line-chart__datesX > .tick > line'
+  );
+  for (let i = 0; i < tickLinesFoundLineChartX.length; i += 1) {
+    tickLinesFoundLineChartX[i].style.display = 'none';
+  }
+};
 
 module.exports = { TickChange };
