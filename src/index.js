c1173fa2b43872dacdf6154ef60bab72ac54f8c9
Nguyen manh <trancongtuanmanh@gmail.com>
2023-04-04 03:22:58 +0700

    add firebase hosting

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ ReactDOM.render(
       {/* add routes without layouts */}
       <Route path="/landing" exact component={Landing} />
       <Route path="/profile" exact component={Profile} />
-      <Route path="/" exact component={Index} />
+      <Route path="/" exact component={Landing} />
       {/* add redirect for first page */}
       <Redirect from="*" to="/" />
     </Switch>
